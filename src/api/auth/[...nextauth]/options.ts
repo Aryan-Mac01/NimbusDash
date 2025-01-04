b9a03ceac1a98c25504f94a7ff57513b4ca9a815
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2025-01-05 02:59:45 +0530

    next-auth initial

---
+++ src/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,45 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs"
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "email"},
+                password: { label: "password", type: "password" }
+            },
+            async authorize(credentials: any): Promise<any>{
+                await dbConnect()
+                try {
+                    const user = await UserModel.findOne({
+                        $or: [
+                            {email: credentials.identifier},
+                            {username : credentials.identifier}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error('No user found with this email')
+                    }
+                    if(!user.isVerified){
+                        throw new Error('Please verify')
+                    }
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+                    if(isPasswordCorrect) {
+                        return user
+                    }
+                    else{
+                        throw new Error('Invalid password')
+                    }
+                } catch (err: any) {
+                    throw new Error(err)
+                }
+            }
+        })
+    ]
+}
